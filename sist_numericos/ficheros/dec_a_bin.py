22edda6f04d2c4fc4cf1fb10ccad563c7816f527
GabrielCourses <statisticscdmx@gmail.com>
2022-05-24 13:45:42 -0500

    add demostracion

--- sist_numericos/ficheros/dec_a_bin.py
+++ sist_numericos/ficheros/dec_a_bin.py
@@ -1,4 +1,4 @@
-# ================================================================================
+# =========================================================================
 # Convertir un número en base 10 a base 2.
 # Paso 1. Hacer la división entera del número en base 10 entre 2.
 # Ejemplos de división entera, es decir, solo obtenemos el cociente entero.
@@ -14,4 +14,7 @@
 # Usamos el algoritmo de la división para probar nuestros resultados
 10 == 4 * (10 // 4) + (10 % 4)
 6 == 3 * (6 // 3) + (6 % 3)
-15 == 2 * (15 // 2) (15 % 2)
+15 == 2 * (15 // 2) + (15 % 2)
+
+
+
