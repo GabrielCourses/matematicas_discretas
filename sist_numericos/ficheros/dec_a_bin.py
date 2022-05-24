68068499e22640208b403d299443752e7c463a4b
GabrielCourses <statisticscdmx@gmail.com>
2022-05-24 12:28:50 -0500

    update README sist_numerico

--- sist_numericos/ficheros/dec_a_bin.py
+++ sist_numericos/ficheros/dec_a_bin.py
@@ -2,8 +2,8 @@
 # Convertir un número en base 10 a base 2.
 # Paso 1. Hacer la división entera del número en base 10 entre 2.
 # Ejemplos de división entera, es decir, solo obtenemos el cociente entero.
-14 // 2
-21 // 2
-8 // 2
+10 // 4
+6 // 3
+15 // 2
 
-
+# Ahora veamos ejemplos del operador % (modulo).
